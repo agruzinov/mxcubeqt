2f9c8fd164e7fb474cd0e06bf30c934ccec68b2e
agruzinov <78498907+agruzinov@users.noreply.github.com>
2023-07-31 17:17:09 +0200

    Added changes from current version

--- mxcubeqt/widgets/task_toolbox_widget.py
+++ mxcubeqt/widgets/task_toolbox_widget.py
@@ -335,6 +335,17 @@ class TaskToolBoxWidget(qt_import.QWidget):
 
             elif isinstance(items[0], queue_item.SampleQueueItem):
                 title = "<b>Sample: %s</b>" % data_model.get_display_name()
+                display_name = data_model.get_display_name()
+                m = re.match("(?P<puckno>\d+)\:(?P<sampleno>\d+)$", display_name)
+                if m:
+                    puckno = int(m.group('puckno'))
+                    sampleno = int(m.group('sampleno'))
+                    base_prefix = "puck%02d_sample%02d" % (puckno, sampleno)
+                else:
+                    base_prefix = display_name
+
+                current_page = self.tool_box.currentWidget()
+                current_page._path_template.base_prefix = base_prefix
             self.method_label.setText(title)
         else:
             self.create_task_button.setEnabled(True)
